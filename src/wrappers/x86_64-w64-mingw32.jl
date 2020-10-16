108b126d84c8472fc20e25ecda191ad3bf541723
jlbuild <juliabuildbot@gmail.com>
2020-10-16 18:54:03 +0000

    Cairo_jll build 1.16.0+6

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -11,94 +11,29 @@ using Xorg_libXext_jll
 using Xorg_libXrender_jll
 using LZO_jll
 using Zlib_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `libcairo`
-const libcairo_splitpath = ["bin", "libcairo-2.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libcairo_path = ""
-
-# libcairo-specific global declaration
-# This will be filled out by __init__()
-libcairo_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libcairo = "libcairo-2.dll"
-
-
-# Relative path to `libcairo_gobject`
-const libcairo_gobject_splitpath = ["bin", "libcairo-gobject-2.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libcairo_gobject_path = ""
-
-# libcairo_gobject-specific global declaration
-# This will be filled out by __init__()
-libcairo_gobject_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libcairo_gobject = "libcairo-gobject-2.dll"
-
-
-# Relative path to `libcairo_script_interpreter`
-const libcairo_script_interpreter_splitpath = ["bin", "libcairo-script-interpreter-2.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libcairo_script_interpreter_path = ""
-
-# libcairo_script_interpreter-specific global declaration
-# This will be filled out by __init__()
-libcairo_script_interpreter_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libcairo_script_interpreter = "libcairo-script-interpreter-2.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Cairo")
+JLLWrappers.@declare_library_product(libcairo, "libcairo-2.dll")
+JLLWrappers.@declare_library_product(libcairo_gobject, "libcairo-gobject-2.dll")
+JLLWrappers.@declare_library_product(libcairo_script_interpreter, "libcairo-script-interpreter-2.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"Cairo")
+    JLLWrappers.@generate_init_header(Glib_jll, Pixman_jll, libpng_jll, Fontconfig_jll, FreeType2_jll, Bzip2_jll, Xorg_libXext_jll, Xorg_libXrender_jll, LZO_jll, Zlib_jll)
+    JLLWrappers.@init_library_product(
+        libcairo,
+        "bin\\libcairo-2.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Glib_jll.PATH_list, Pixman_jll.PATH_list, libpng_jll.PATH_list, Fontconfig_jll.PATH_list, FreeType2_jll.PATH_list, Bzip2_jll.PATH_list, Xorg_libXext_jll.PATH_list, Xorg_libXrender_jll.PATH_list, LZO_jll.PATH_list, Zlib_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Glib_jll.LIBPATH_list, Pixman_jll.LIBPATH_list, libpng_jll.LIBPATH_list, Fontconfig_jll.LIBPATH_list, FreeType2_jll.LIBPATH_list, Bzip2_jll.LIBPATH_list, Xorg_libXext_jll.LIBPATH_list, Xorg_libXrender_jll.LIBPATH_list, LZO_jll.LIBPATH_list, Zlib_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libcairo_gobject,
+        "bin\\libcairo-gobject-2.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libcairo_path = normpath(joinpath(artifact_dir, libcairo_splitpath...))
+    JLLWrappers.@init_library_product(
+        libcairo_script_interpreter,
+        "bin\\libcairo-script-interpreter-2.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libcairo_handle = dlopen(libcairo_path)
-    push!(LIBPATH_list, dirname(libcairo_path))
-
-    global libcairo_gobject_path = normpath(joinpath(artifact_dir, libcairo_gobject_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libcairo_gobject_handle = dlopen(libcairo_gobject_path)
-    push!(LIBPATH_list, dirname(libcairo_gobject_path))
-
-    global libcairo_script_interpreter_path = normpath(joinpath(artifact_dir, libcairo_script_interpreter_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libcairo_script_interpreter_handle = dlopen(libcairo_script_interpreter_path)
-    push!(LIBPATH_list, dirname(libcairo_script_interpreter_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
